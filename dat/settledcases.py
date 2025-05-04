d6db4beec280f4f26b2d5c2ec2306b10c51083d7
Dan Roth <dw-roth@users.noreply.github.com>
2025-05-04 10:42:25 +0200

    Initial checkin

---
+++ dat/settledcases.py
@@ -0,0 +1,7 @@
+settled_cases = [
+    "847619", # settled
+    "850962", # settled
+    "868420"  # supplemental brief which was accepted by prosector with
+              # recommendation appellate court remand to trial court based
+              # on law cited in supplemental brief
+]
