72a00ba65d1ccaa1cc7fb99abe7edf51f306421e
Voy Lento <203371805+voylento@users.noreply.github.com>
2025-05-04 11:27:33 +0200

    removed intermediate files

--- dat/settledcases.py
+++ dat/settledcases.py
@@ -1,7 +1,8 @@
 settled_cases = [
+    "852680",
     "847619", # settled
     "850962", # settled
-    "868420"  # supplemental brief which was accepted by prosector with
+    "868420", # supplemental brief which was accepted by prosector with
               # recommendation appellate court remand to trial court based
               # on law cited in supplemental brief
 ]
