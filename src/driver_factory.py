ea6a12343b92acc2cfe419f01005a7f97b995da1
voylento <203371805+voylento@users.noreply.github.com>
2025-07-22 22:58:33 +0000

    updates from testing on Ubuntu

--- src/driver_factory.py
+++ src/driver_factory.py
@@ -37,10 +37,12 @@ def create_driver(project_root=None, headless=True):
     # Build Chrome options
     options = Options()
     options.binary_location = chrome_binary
-    if headless:
-        options.add_argument("--headless=new")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-software-rasterizer")
+    options.add_argument("--remote-debugging-port=9222")
 
     service = Service(driver_binary)
     driver = webdriver.Chrome(service=service, options=options)
