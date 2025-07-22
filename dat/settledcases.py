d5aa2ed149fc748f31d8a16e7589373245d115fe
voylento <203371805+voylento@users.noreply.github.com>
2025-07-22 16:07:20 -0700

    remove redundant dat directory

--- dat/settledcases.py
+++
@@ -1,8 +0,0 @@
-settled_cases = [
-    "847619", # settled
-    "850962", # settled
-    "852680", # case withdrawn
-    "868420"  # supplemental brief which was accepted by prosector with
-              # recommendation appellate court remand to trial court based
-              # on law cited in supplemental brief
-]
